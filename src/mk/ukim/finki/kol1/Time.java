6fef36f0f63a35e0bf9973411f6e28e5d55bc7e5
Kostadin Krstev <krstev.kostadin@gmail.com>
2017-11-16 22:49:04 +0100

    simplified the implementation

--- src/mk/ukim/finki/kol1/Time.java
+++ src/mk/ukim/finki/kol1/Time.java
@@ -1,37 +1,35 @@
 package mk.ukim.finki.kol1;
 
-class Time {
+class Time implements Comparable<Time> {
     private int hour;
-    private int minute;
+    private int min;
 
-    public Time(int hour, int minute) {
+    public Time(int hour, int min) throws InvalidTimeException {
+        if (hour < 0 || hour > 23 || min < 0 || min > 59)
+            throw new InvalidTimeException(String.format("%d %d", hour, min));
         this.hour = hour;
-        this.minute = minute;
-    }
-
-    public int getHour() {
-        return hour;
-    }
-
-    public int getMinute() {
-        return minute;
+        this.min = min;
     }
 
     @Override
-    public String toString() {
-        return String.format("%2d:%02d", hour, minute);
+    public int compareTo(Time o) {
+        int thisMin = this.hour * 60 + this.min;
+        int oMin = o.hour * 60 + o.min;
+        return Integer.compare(thisMin, oMin);
     }
 
-    public String toStringAMPM() {
+    public String toString(TimeFormat format) {
+        if (format.toString().equals(TimeFormat.FORMAT_24.toString()))
+            return String.format("%2d:%02d", hour, min);
         String part = "AM";
-        if (hour == 0)
+        if (hour >= 0 && hour < 1)
             hour += 12;
-        else if (hour == 12)
+        if (hour >= 12 && hour < 13)
             part = "PM";
-        else if (hour > 12) {
+        if (hour >= 13 && hour < 24) {
             part = "PM";
             hour -= 12;
         }
-        return String.format("%2d:%02d %s", hour, minute, part);
+        return String.format("%2d:%02d %s", hour, min, part);
     }
 }

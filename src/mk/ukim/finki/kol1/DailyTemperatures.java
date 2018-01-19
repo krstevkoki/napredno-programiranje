97405a9983062f3992590569a945f00a7545592b
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-01-19 19:07:22 +0100

    modified methods to use Java 8 Stream API, changed ArrayList to List

--- src/mk/ukim/finki/kol1/DailyTemperatures.java
+++ src/mk/ukim/finki/kol1/DailyTemperatures.java
@@ -3,9 +3,11 @@ package mk.ukim.finki.kol1;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class DailyTemperatures {
-    private ArrayList<Temperature> temperatures;
+    private List<Temperature> temperatures;
 
     public DailyTemperatures() {
         temperatures = new ArrayList<>();
@@ -16,7 +18,7 @@ class DailyTemperatures {
             String line;
             while ((line = bf.readLine()) != null && line.length() != 0) {
                 String[] parts = line.split("\\s+");
-                ArrayList<Double> temps = new ArrayList<>();
+                List<Double> temps = new ArrayList<>();
                 for (int i = 1; i < parts.length; ++i)
                     temps.add(Double.parseDouble(parts[i].substring(0, parts[i].length() - 1)));
                 if (parts[1].endsWith("C"))
@@ -29,45 +31,45 @@ class DailyTemperatures {
 
     private void convertToCelsius(Temperature t) {
         if (t.getScale() == 'F') {
-            for (int i = 0; i < t.getTemperatures().size(); ++i) {
-                Double temperature = ((t.getTemperatures().get(i) - 32.0) * 5.0) / 9.0;
-                t.getTemperatures().set(i, temperature);
-            }
+            t.setTemperatures(
+                    t.getTemperatures().stream()
+                            .map(temp -> ((temp - 32.0) * 5.0) / 9.0)
+                            .collect(Collectors.toList())
+            );
             t.setScale('C');
         }
     }
 
     private void convertToFahrenheit(Temperature t) {
         if (t.getScale() == 'C') {
-            for (int i = 0; i < t.getTemperatures().size(); ++i) {
-                Double temperature = (t.getTemperatures().get(i) * 9.0) / 5.0 + 32.0;
-                t.getTemperatures().set(i, temperature);
-            }
+            t.setTemperatures(
+                    t.getTemperatures().stream()
+                    .map(temp -> (temp * 9.0) / 5.0 + 32.0)
+                    .collect(Collectors.toList())
+            );
             t.setScale('F');
         }
     }
 
     private Double minTemp(Temperature t) {
-        Double min = Double.MAX_VALUE;
-        for (int i = 0; i < t.getTemperatures().size(); ++i)
-            if (min > t.getTemperatures().get(i))
-                min = t.getTemperatures().get(i);
-        return min;
+        return t.getTemperatures().stream()
+                .mapToDouble(Double::valueOf)
+                .min()
+                .orElse(Double.MIN_VALUE);
     }
 
     private Double maxTemp(Temperature t) {
-        Double max = (double) Integer.MIN_VALUE;
-        for (int i = 0; i < t.getTemperatures().size(); ++i)
-            if (max < t.getTemperatures().get(i))
-                max = t.getTemperatures().get(i);
-        return max;
+        return t.getTemperatures().stream()
+                .mapToDouble(Double::valueOf)
+                .max()
+                .orElse(Double.MAX_VALUE);
     }
 
-    private Float averageTemp(Temperature t) {
-        Double average = 0d;
-        for (int i = 0; i < t.getTemperatures().size(); ++i)
-            average += t.getTemperatures().get(i);
-        return (float) (average / t.getTemperatures().size());
+    private Double averageTemp(Temperature t) {
+        return t.getTemperatures().stream()
+                .mapToDouble(Double::valueOf)
+                .average()
+                .orElse(Double.MIN_VALUE);
     }
 
     private int count(Temperature t) {
@@ -84,7 +86,7 @@ class DailyTemperatures {
                 convertToFahrenheit(temperature);
             Double maxTemp = maxTemp(temperature);
             Double minTemp = minTemp(temperature);
-            Float averageTemp = averageTemp(temperature);
+            Double averageTemp = averageTemp(temperature);
             Integer count = count(temperature);
             Integer day = temperature.getDay();
             writer.println(String.format("%3s: Count: %3d Min: %6.2f%c Max: %6.2f%c Avg: %6.2f%c", day, count,
